dca724902cc9177e6627eb66a4b0cd9eb2644322
Erik <erikhgm@gmail.com>
2022-09-26 15:38:22 +0000

    Create: menu_page view and url

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('menu/', views.menu_page, name='menu'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def menu_page(request):
+    return render(request, 'menu_page.html')
