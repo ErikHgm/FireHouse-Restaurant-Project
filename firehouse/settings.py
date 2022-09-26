4ca701e2e2a7a36f7392855e0028d2753ae174ce
Erik <erikhgm@gmail.com>
2022-09-26 10:41:45 +0000

    Update: allowed_hosts in settings.py

--- firehouse/settings.py
+++ firehouse/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['firehouse-restaurant.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['firehouse-restaurant.herokuapp.com', 'localhost']
 
 
 # Application definition
