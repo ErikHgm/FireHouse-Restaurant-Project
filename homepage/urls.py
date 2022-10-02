c396ecc09e0e6a974202e09ea1adf1d44c618f95
Erik <erikhgm@gmail.com>
2022-10-02 20:09:21 +0000

    Add: Booking url path and links on base/index templates

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu_page, name='menu'),
+    path('booking/', views.booking_page, name='booking_page'),
 ]
