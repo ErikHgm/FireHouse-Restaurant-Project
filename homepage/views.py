02bf1d25b06eddb46a77b47563ab7ccc0a0713ba
Erik <erikhgm@gmail.com>
2022-10-08 21:10:32 +0000

    docs: add docstrings to all views

--- homepage/views.py
+++ homepage/views.py
@@ -5,10 +5,18 @@ from django.contrib import messages
 
 
 def home(request):
+    """The view for the start page. Renders the index.html
+    page which also extends the base.html
+    """
     return render(request, 'index.html')
 
 
 def booking_page(request):
+    """The view for the booking page. If user is logged in it renders the
+    booking.html, otherwise it renders the page to login or signup.
+    When user has made a booking it redirects to mybooking overview page.
+    It takes the input from the form and store it in the Booking model.
+    """
     if request.method == 'POST':
         form = BookingForm(data=request.POST)
         if form.is_valid():
@@ -25,6 +33,10 @@ def booking_page(request):
 
 
 def mybookings_page(request):
+    """The view that renders the mybookings.html which shows all
+    current booking by the user. Checks if user is logged in
+    otherwise it redirects to the signup page.
+    """
     if request.user.is_authenticated:
         bookings = Booking.objects.filter(user=request.user)
         context = {
@@ -34,7 +46,13 @@ def mybookings_page(request):
     else:
         return redirect('../accounts/signup')
 
+
 def edit_booking(request, booking_id):
+    """The view that renders the edit_booking page where the user can
+    update a current booking. Checks if current user matches the user
+    that made the booking, otherwise it redirects to the mybookings_page.
+    Uses the form data from the user to update the booking in the database.
+    """
     booking = get_object_or_404(Booking, id=booking_id)
     if request.user != booking.user:
         return redirect('mybookings_page')
@@ -52,6 +70,10 @@ def edit_booking(request, booking_id):
 
 
 def delete_booking(request, booking_id):
+    """The view that performs the deletion of a booking.
+    Checks if current user matches the user that made the booking,
+    otherwise it redirects to the mybookings_page.
+    """
     booking = get_object_or_404(Booking, id=booking_id)
     if request.user != booking.user:
         return redirect('mybookings_page')
